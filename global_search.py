fbb7c792731fc22032694ce8c860f14d3a3d1a58
Kahaf <107896019+mkbhuiyan96@users.noreply.github.com>
2023-07-12 08:59:11 -0400

    Update global_search.py
    Updated to handle errors better, and use a persistent session instead of constantly creating a new one

--- global_search.py
+++ global_search.py
@@ -1,11 +1,10 @@
 import time
 import re
-from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import RequestException
+from bs4 import BeautifulSoup
 
-def main():
-    class_url = input('Enter the URL of the class page from CUNY Global Search: ')
+def create_session():
     payload = {
         'selectedInstName': 'Queens College | ',
         'inst_selection': 'QNS01',
@@ -13,25 +12,35 @@ def main():
         'term_value': '1239',
         'next_btn': 'Next'
     }
-    
     while True:
         try:
-            with requests.session() as session:
-                session.post('https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController', data=payload)
-                response = session.get(class_url)
-                soup = BeautifulSoup(response.content, 'html.parser')
-                
+            session = requests.session()
+            session.post('https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController', data=payload, timeout=10)
+            return session
+        except (RequestException, requests.Timeout):
+            print("Error occurred while starting a session, trying again in 5 minutes.")
+            time.sleep(300)
+
+def main():
+    class_url = input('Enter the URL of the class page from CUNY Global Search: ')
+    session = create_session()
+
+    while True:
+        try:
+            response = session.get(class_url, timeout=10)
+            soup = BeautifulSoup(response.content, 'html.parser')
             full_class_name = soup.find('span', {'id': 'DERIVED_CLSRCH_DESCR200'}).text.strip()
+            
             match = re.search(r'([A-Z]+\s\d+)', full_class_name)
             if match:
                 class_name = match.group(1)
             else:
-                class_name = full_class_name  # If the regex doesn't match, use the full name
+                class_name = full_class_name
             status = soup.find('span', {'id': 'SSR_CLS_DTL_WRK_SSR_DESCRSHORT'}).text
             print(f'{class_name}: {status}')
-        except RequestException:
-            print("Error occured, trying again...")
-        time.sleep(3)
+            time.sleep(3)
+        except (RequestException, requests.Timeout):
+            session = create_session()
     
 if __name__ == '__main__':
     main()
